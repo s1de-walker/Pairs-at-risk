7cdf9358fdca911c235f53687b0625451321f041
Sujay <s1dewalker23@gmail.com>
2025-03-26 19:29:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -507,8 +507,8 @@ if st.session_state.pairs:
 
     # Range Ratio
     # ------------------------------------
-    with st.expander(f"Range Ratio"):
-        st.write("Range Ratio")
+    with st.expander(f"Range Difference"):
+        #st.write("Range Ratio")
         #st.dataframe(data_high)
         #st.dataframe(data_low)
         range1 = data_high[ticker1] - data_low[ticker1]
