660624940d0c60b47dca51b689f38850a2d27bbb
Sujay <s1dewalker23@gmail.com>
2025-07-13 17:10:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -50,19 +50,19 @@ st.caption(f"Selected period: **{(end_date.year - start_date.year) * 12 + (end_d
 date_range_days = (end_date - start_date).days  # Calculate total available days
 
 st.write("")
-st.subheader("Enter the units and tickers")
+st.subheader("Enter the allocation and tickers")
 
 # Form for user input
 
 with st.form("pairs_form"):
     col1, col2, col3, col4, col5 = st.columns([2, 3, 1, 2, 3])
     
-    units1 = col1.number_input("Units", min_value=1, step=1, key="units1")
+    units1 = col1.number_input("Lots", min_value=1, step=1, key="units1")
     ticker1 = col2.text_input("Stock/ETF 1").upper()
     with col3:
         st.write("")
         st.markdown("<p style='text-align: center; font-size: 24px;'>_</p>", unsafe_allow_html=True)
-    units2 = col4.number_input("Units", min_value=1, step=1, key="units2")
+    units2 = col4.number_input("Lots", min_value=1, step=1, key="units2")
     ticker2 = col5.text_input("Stock/ETF 2").upper()
     
     submit = st.form_submit_button("Confirm Pair")
